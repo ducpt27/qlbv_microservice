12b1786b21598e287c32e0b12b6850e47c18fdf4
Đinh Việt Anh <vietanh2150@gmail.com>
2021-04-21 17:17:59 +0700

    update route

---
+++ DTO/Request/Route/AddRouteReq.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using MediatR;
+
+namespace VeXe.DTO.Request.Route
+{
+    public class AddRouteReq : IRequest<RouteDto>
+    {
+        [Required]
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [Required]
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+        
+        [JsonPropertyName("origin_id")]
+        public int OriginId { get; set; }
+        
+        public int[] PointIds  { get; set; }
+    }
+}
--- DTO/Request/Route/EditRouteReq.cs
+++ DTO/Request/Route/EditRouteReq.cs
@@ -1,25 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using MediatR;
-using VeXe.DTO;
 
-namespace VeXe.Dto.Request
+namespace VeXe.DTO.Request.Route
 {
-    public class RouteReq : IRequest<RouteDto>
+    public class EditRouteReq : IRequest<RouteDto>
     {
-        [Required]
         [JsonPropertyName("name")]
         public string Name { get; set; }
 
         [Required]
+        public int Id { get; set; }
+        
         [JsonPropertyName("status")]
         public int Status { get; set; }
         
         [JsonPropertyName("origin_id")]
         public int OriginId { get; set; }
-        
-        public int[] PointIds  { get; set; }
     }
 }
