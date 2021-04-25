8b256bf176847dee6aa561c93878973515c828fe
Trung Đức Phạm <44116502+ducpt27@users.noreply.github.com>
2021-04-25 16:20:53 +0700

    Update Point.cs

--- Domain/Point.cs
+++ Domain/Point.cs
@@ -25,9 +25,6 @@ namespace VeXe.Domain
         [Column("ward_id")]
         public int WardId { get; set; }
         
-        [Column("name")]
-        public string Name { get; set; }
-        
         [Column("status")]
         public int Status { get; set; }
         
